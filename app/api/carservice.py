3c511fe882ea285729bd93e7681efdfa7a2eeccd
Seclud <82933148+Seclud@users.noreply.github.com>
2024-06-19 00:05:51 +0500

    remove old code

--- app/api/carservice.py
+++ app/api/carservice.py
@@ -7,30 +7,23 @@ from app.models import CarService, CarServiceCreate, CarServicePublic, CarServic
 
 router = APIRouter()
 
-@router.get("/", response_model=CarServicesPublic)
-def read_services(session: SessionDep, skip: int = 0, limit: int = 100):
 
-    # if current_user.is_superuser:
-    #     count_statement = select(func.count()).select_from(CarService)
-    #     count = session.exec(count_statement).one()
-    #     statement = select(CarService).offset(skip).limit(limit)
-    #     services = session.exec(statement).all()
-    # else:
+@router.get("/", response_model=CarServicesPublic)
+def read_services(current_user: CurrentUser, session: SessionDep, skip: int = 0, limit: int = 100):
     count_statement = (
         select(func.count())
         .select_from(CarService)
-        # .where(CarService.owner_id == current_user.id)
     )
     count = session.exec(count_statement).one()
     statement = (
         select(CarService)
         .offset(skip)
-        # .where(CarService.owner_id == current_user.id)
         .limit(limit)
     )
     services = session.exec(statement).all()
 
     return CarServicesPublic(data=services, count=count)
+
 
 @router.get("/{id}", response_model=CarServicePublic)
 def read_service(session: SessionDep, current_user: CurrentUser, id: int):
@@ -53,7 +46,6 @@ def create_service(*, session: SessionDep, current_user: CurrentUser, service_in
 
 @router.put("/{id}", response_model=CarServicePublic)
 def update_service(*, session: SessionDep, current_user: CurrentUser, id: int, service_in: CarServiceUpdate):
-
     service = session.get(CarService, id)
     if not service:
         raise HTTPException(status_code=404, detail="CarService not found")
@@ -74,13 +66,12 @@ def delete_service(session: SessionDep, current_user: CurrentUser, id: int) -> M
         raise HTTPException(status_code=404, detail="CarService not found")
     if not current_user.is_superuser and (service.owner_id != current_user.id):
         raise HTTPException(status_code=400, detail="Not enough permissions")
-    
-    
+
     lifts = session.query(Lift).filter(Lift.carservice_id == id).all()
     for lift in lifts:
         session.query(Booking).filter(Booking.lift_id == lift.id).delete()
         session.delete(lift)
-    
+
     session.delete(service)
     session.commit()
     return Message(message="Service deleted successfully")
