82d4555425006c8a5356d160e8333798f704c7f5
Seclud <82933148+Seclud@users.noreply.github.com>
2024-06-19 18:29:43 +0500

    Make redirect to /garages instead of opening homepage

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import {Navigate} from "react-router-dom";
 import { Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import Navbar from './Navbar';
 
 function HomePage() {
-    const { isAuthenticated, logout } = useAuth();
+    // const { isAuthenticated, logout } = useAuth();
 
-    const handleLogout = () => {
-        logout();
-    }; 
+    // const handleLogout = () => {
+    //     logout();
+    // }; 
 
-    return (
-        <div>
-        <Navbar />
-        <h1>Welcome to the App</h1>
-        <nav>
-        <ul>
-            {!isAuthenticated ? (
-            <>
-              <li><Link to="/login">Login</Link></li>
-              <li><Link to="/register">Register</Link></li>
-            </>
-            ) : (
-                <>
-                <li><Link to="/garages">Garages</Link></li>
-                <li><button onClick={handleLogout}>Logout</button></li>
-                </>
-            )}
-            </ul>
-        </nav>
-        </div>
-    );
-    }
+    // return (
+    //     <div>
+    //     <Navbar />
+    //     <h1>Welcome to the App</h1>
+    //     <nav>
+    //     <ul>
+    //         {!isAuthenticated ? (
+    //         <>
+    //           <li><Link to="/login">Login</Link></li>
+    //           <li><Link to="/register">Register</Link></li>
+    //         </>
+    //         ) : (
+    //             <>
+    //             <li><Link to="/garages">Garages</Link></li>
+    //             <li><button onClick={handleLogout}>Logout</button></li>
+    //             </>
+    //         )}
+    //         </ul>
+    //     </nav>
+    //     </div>
+    // );
+    // }
+    return <Navigate to="/garages" replace/>
+}
 
 export default HomePage;
