d5f1c2571d80fd69f2df3ba89eb565053d1b06b9
Seclud <82933148+Seclud@users.noreply.github.com>
2024-06-19 18:16:52 +0500

    Add new components with new UI

---
+++ frontend/src/components/BookingModal.jsx
@@ -0,0 +1,156 @@
+import React, {useEffect, useState} from 'react';
+import {addMinutes, formatISO} from 'date-fns';
+import {serverURL} from "../config";
+import {Button, Modal, MultiSelect, Space, Stack, Text, Title} from "@mantine/core";
+import {DateTimePicker} from '@mantine/dates';
+import {notifications} from "@mantine/notifications";
+import {useNavigate} from "react-router-dom";
+import ReactSelect from 'react-select';
+
+
+export default function BookingModal(props) {
+    const navigate = useNavigate()
+    const [services, setServices] = useState([]);
+    const [selectedServiceIds, setSelectedServiceIds] = useState([]);
+    const options = services.map(service => ({
+        value: `${service.id}`,
+        label: `${service.description} (${service.duration} минут)`
+    }));
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const [bookingData, setBookingData] = useState({
+        lift_id: props.liftId,
+        time_from: '',
+        time_to: '',
+    });
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            const response = await fetch(`${serverURL}/services`);
+            const data = await response.json();
+            setServices(data);
+        };
+        fetchServices();
+    }, []);
+
+    const calculateAndSetTimeTo = (newSelectedServiceIds) => {
+            const totalDuration = newSelectedServiceIds.reduce((acc, serviceId) => {
+                const service = services.find(service => service.id === serviceId);
+                return acc + (service ? service.duration : 0);
+            }, 0);
+
+            if (bookingData.time_from) {
+                const timeFrom = new Date(bookingData.time_from);
+                const timeTo = addMinutes(timeFrom, totalDuration);
+                const formattedTimeTo = formatISO(timeTo, {representation: 'complete'});
+                const formattedTimeToCorrected = formattedTimeTo.slice(0, 19)
+                console.log(timeFrom, timeTo, formattedTimeToCorrected)
+                setBookingData({...bookingData, time_to: formattedTimeToCorrected});
+            }
+        }
+    ;
+
+    const handleChange = (selectedOptions) => {
+        const newSelectedServiceIds = selectedOptions.map(option => parseInt(option.value));
+        setSelectedServiceIds(newSelectedServiceIds);
+        calculateAndSetTimeTo(newSelectedServiceIds);
+    };
+
+    const handleTimeChange = (name, value) => {
+        setBookingData({...bookingData, [name]: value});
+    };
+
+    useEffect(() => {
+        if (bookingData.time_from)
+            calculateAndSetTimeTo(selectedServiceIds);
+    }, [bookingData.time_from]);
+
+    useEffect(() => {
+        setBookingData({...bookingData, lift_id: props.liftId});
+    }, [props.liftId]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${serverURL}/bookings/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+                },
+                body: JSON.stringify({
+                    booking: {
+                        status: "await_confirm",
+                        time_from: bookingData.time_from,
+                        time_to: bookingData.time_to,
+                        lift_id: parseInt(bookingData.lift_id),
+                    },
+                    service_ids: selectedServiceIds.map(service => parseInt(service)),
+                }),
+            });
+            if (!response.ok) {
+                const errorData = await response.json();
+                const errorMessage = errorData.detail || 'An error occurred while creating the booking.';
+                throw new Error(errorMessage);
+            }
+            setErrorMessage('');
+            console.log('Booking created successfully:', await response.data);
+            notifications.show({
+                title: 'Вы успешно забронировали подъёмник',
+            })
+            props.setIsOpen(false)
+        } catch (error) {
+            console.error('Error creating booking:', error.message);
+            setErrorMessage(error.message);
+        }
+    };
+
+    return (
+        <Modal opened={props.isOpen} onClose={() => props.setIsOpen(false)}>
+            <Stack
+                align="stretch"
+                justify="flex-start"
+                gap="md"
+            >
+                <Title order={2} ta="center" mt="md">Забронировать подъёмник</Title>
+                {/* <MultiSelect
+                    label="Услуги"
+                    placeholder="Выберите услуги"
+                    data={options}
+                    value={selectedServiceIds}
+                    onChange={handleChange}
+                /> */}
+                <ReactSelect
+                options={options}
+                isMulti
+                onChange={handleChange}
+                />
+                <DateTimePicker
+                    label="Время начала"
+                    placeholder="Выберите дату и время начала записи"
+                    value={bookingData.time_from}
+                    onChange={(value) => handleTimeChange("time_from", value)}
+                />
+                {/* <DateTimePicker
+                    label="Время окончания"
+                    value={bookingData.time_to && new Date(bookingData.time_to)}
+                    disabled
+                /> */}
+                <Text>
+                    Время окончания: {selectedServiceIds.length > 0 && bookingData.time_to ? new Date(bookingData.time_to).toLocaleString() : 'Выберите услуги и время начала'}
+                </Text>
+
+                {errorMessage &&
+                    <>
+                        <Space h='md'/>
+                        <Text c='red'>{errorMessage}</Text>
+                    </>
+                }
+
+                <Button onClick={handleSubmit} disabled={!bookingData.time_to || selectedServiceIds.length === 0}>
+                    Забронировать
+                </Button>
+            </Stack>
+        </Modal>
+    );
+}
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {AppShell, Button, Group} from '@mantine/core';
+import styles from './Layout.module.css'
+import {Link, Outlet} from "react-router-dom";
+import {useAuth} from "../AuthContext";
+
+export default function Layout() {
+    const {isAuthenticated, isAdmin, logout} = useAuth();
+
+    return (
+        <AppShell header={{height: 74}}>
+            <AppShell.Header>
+                <div className={styles.header}>
+                    {/* <Link to="/garages">
+                        <img alt="logo" src={Logo} className={styles.logo}/>
+                    </Link> */}
+                    {
+                        isAuthenticated &&
+                        <Group>
+                            <Link to="garages">
+                                <Button variant="subtle" color="rgba(82, 82, 82, 1)" size="md">Автосервисы</Button>
+                            </Link>
+                            <Link to="bookings">
+                                <Button variant="subtle" color="rgba(82, 82, 82, 1)" size="md">Мои записи</Button>
+                            </Link>
+                            {
+                                isAdmin &&
+                                <Link to="all-bookings">
+                                    <Button variant="subtle" color="rgba(82, 82, 82, 1)" size="md">Все записи</Button>
+                                </Link>
+                            }
+                        </Group>
+                    }
+                    {
+                        !isAuthenticated &&
+                        <Group>
+                            <Link to="login">
+                                <Button variant="subtle" color="gray" size="md">Авторизоваться</Button>
+                            </Link>
+                            <Link to="registration">
+                                <Button variant="filled" color="indigo" size="md">Зарегистрироваться</Button>
+                            </Link>
+                        </Group>
+                    }
+                    {
+                        isAuthenticated &&
+                        <Group>
+                            <Button variant="subtle" color="gray" size="md" onClick={() => logout()}>Выйти</Button>
+                        </Group>
+                    }
+                </div>
+            </AppShell.Header>
+
+            <AppShell.Main>
+                <Outlet/>
+            </AppShell.Main>
+        </AppShell>
+    );
+};
