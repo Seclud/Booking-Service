09112738dd46fa6837f28d20a8e28830175692cc
Seclud <82933148+Seclud@users.noreply.github.com>
2024-06-19 18:15:21 +0500

    Add mantine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,27 +9,38 @@ import GaragesPage from './GaragesPage';
 import GarageDetailPage from './GarageDetailPage';
 import LiftBookingPage from './LiftBookingPage';
 import BookingsPage from './BookingsPage.js';
-import CreateCarServicePage from './CreateCarServicePage';
+import CreateGaragePage from './CreateGaragePage.js';
 import LiftCreationPage from './LiftCreationPage';
+import AllBookingsPage from './AllBookingsPage.js'
+import {MantineProvider} from "@mantine/core";
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import {Notifications} from "@mantine/notifications";
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/garages" element={<GaragesPage />} />
-          <Route path="/carservices/:id" element={<GarageDetailPage/>} />
-          <Route path="/book/lift/:liftId" element={<LiftBookingPage />} />
-          <Route path="/mybookings" element={<BookingsPage />} />
-          <Route path="/carservices/create" element={<CreateCarServicePage />} />
-          <Route path="/lifts/create/:serviceId" element={<LiftCreationPage />} />
-          {/* Add other routes here */}
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <MantineProvider>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/registration" element={<RegistrationPage />} />
+              <Route path="/garages" element={<GaragesPage />} />
+              <Route path="/garage/:id" element={<GarageDetailPage/>} />
+              <Route path="/book/lift/:liftId" element={<LiftBookingPage />} />
+              <Route path="/bookings" element={<BookingsPage />} />
+              <Route path="/garage/create" element={<CreateGaragePage />} />
+              <Route path="/lifts/create/:serviceId" element={<LiftCreationPage />} />
+              <Route path="/all-bookings" element={<AllBookingsPage />} />            
+            </Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </MantineProvider>
   );
 }
 
