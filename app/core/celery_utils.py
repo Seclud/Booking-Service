f16ab169d353e2ebd8beb95e48356684fc866557
Seclud <82933148+Seclud@users.noreply.github.com>
2024-06-20 15:24:54 +0500

    Update email reminder

--- app/core/celery_utils.py
+++ app/core/celery_utils.py
@@ -5,7 +5,7 @@ from sqlmodel import select, Session, func
 from app.api.deps import SessionDep
 from datetime import timedelta, date
 from celery.schedules import crontab
-from app.models import Booking
+from app.models import Booking, CarService, Lift
 from .database import engine
 from .config import settings
 
@@ -16,7 +16,7 @@ app = Celery('tasks', broker=f'pyamqp://{settings.RABBIT_USER}:{settings.RABBIT_
 def send_email(email_data):
     msg = MIMEText(email_data["body"])
     msg["Subject"] = email_data["subject"]
-    msg["From"] = settings.EMAILS_FROM_EMAIL
+    msg["From"] = "BookingService"
     msg["To"] = email_data["to"]
 
     try:
@@ -35,22 +35,28 @@ def send_booking_reminder():
         current_date = date.today()
         tomorrow = current_date + timedelta(days=1)
 
-        statement = select(Booking).where(func.date(Booking.time_from) == tomorrow)
-        bookings = session.exec(statement).all()
+        statement = (
+            select(Booking, CarService)
+            .join(Lift, Lift.id == Booking.lift_id)
+            .join(CarService, CarService.id == Lift.carservice_id)
+            .where(func.date(Booking.time_from) == tomorrow)
+        )
+        results = session.exec(statement).all()
 
-        for booking in bookings:
+        for booking, car_service in results:
+            formatted_time = booking.time_from.strftime("%H:%M")
             email_data = {
-                "subject": "Booking Reminder",
-                "body": f"Your booking is scheduled for tomorrow at {booking.time_from.time()}",
-                #"to": booking.owner.email
-                "to": "tihegr@rambler.ru"
+                "subject": "Напоминание о записи",
+                "body": f"У вас запланирована запись на завтра в {formatted_time} в сервисе {car_service.name}",
+                "to": booking.owner.email
+                #"to": "tihegr@rambler.ru"
             }
             send_email.delay(email_data)
 
 app.conf.beat_schedule = {
     'send-booking-reminder-every-day': {
         'task': 'send_booking_reminder',
-        'schedule': crontab(hour="17"),
+        'schedule': crontab(hour="12"),
     },
 }
 app.conf.timezone = 'Europe/Moscow'
